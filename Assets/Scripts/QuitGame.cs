e7a53d4681ff4e48b9127b958a181f344e2fc493
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-10 18:54:11 -0400

    no message

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown("left ctrl") || Input.GetKeyDown("right ctrl"))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void OnQuitButtonPress()
+    {
+        Application.Quit();
+    }
+}
