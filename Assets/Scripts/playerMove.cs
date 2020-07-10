1c94067b398305e9ebd6c569fb72017ced26c06c
clarence112 <clarencepacelli@gmail.com>
2020-07-10 18:11:13 -0400

    no message

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+    public static float force = 2.0f;
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rb.AddForce(Input.GetAxis("Horizontal") * force, 0f, Input.GetAxis("Vertical") * force);
+    }
+}
