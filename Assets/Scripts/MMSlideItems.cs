d1ccd0efc4253af36f3bac5a60ad7e71f593e95e
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-10 23:32:37 -0400

    no message

---
+++ Assets/Scripts/MMSlideItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMSlideItems : MonoBehaviour
+{
+    public float distanceToMove;
+    public string directionToMove;
+    public string menuElementType;
+    public Vector3 originalLocation;
+    public static string menuState;
+
+    void Start()
+    {
+        originalLocation = transform.position;
+        menuState = "Main";
+    }
+
+    void Update()
+    {
+        if (menuState == "Main")
+        {
+            if (menuElementType == "Main")
+            {
+                transform.position += new Vector3((transform.position.x + originalLocation.x) / 100, 0, 0);
+            }
+        }
+    }
+}
