c3fbbd154487e94fea3ad754450ccdeaecba5aad
clarence112 <clarencepacelli@gmail.com>
2020-07-11 15:33:13 -0400

    no message

---
+++ Assets/Scripts/musicRand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicRand : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public AudioSource a0;
+    public AudioSource a1;
+    public AudioSource a2;
+    public AudioSource a3;
+    public AudioSource a4;
+
+    void Start() {
+        switch ((int)Mathf.Round(Random.Range(0, 4))) {
+            case 0:
+                a0.Play();
+                break;
+            case 1:
+                a1.Play();
+                break;
+            case 2:
+                a2.Play();
+                break;
+            case 3:
+                a3.Play();
+                break;
+            case 4:
+                a4.Play();
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
