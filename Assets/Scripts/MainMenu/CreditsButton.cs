edb70e37a6773717fa66b372a4031322c84b0d1d
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 10:37:39 -0400

    no message

---
+++ Assets/Scripts/MainMenu/CreditsButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite inactiveButton;
+    public Sprite hoveringButton;
+    public Sprite pressedButton;
+    private bool pressed;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        if (!pressed)
+        {
+            spriteRenderer.sprite = hoveringButton;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = inactiveButton;
+    }
+
+    void OnMouseDown()
+    {
+        pressed = true;
+        spriteRenderer.sprite = pressedButton;
+    }
+
+    void OnMouseUp()
+    {
+        spriteRenderer.sprite = hoveringButton;
+        pressed = false;
+    }
+}
