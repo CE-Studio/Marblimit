38f9aff109072fb31d1be06e67caf81d4df5efe7
clarence112 <clarencepacelli@gmail.com>
2020-07-11 11:06:58 -0400

    no message

--- Assets/Scripts/boost.cs
+++ Assets/Scripts/boost.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class boost : MonoBehaviour {
     // Start is called before the first frame update
     private float turn = 0f;
+    public static float stren = 10f;
 
     void Start() {
         
@@ -12,12 +13,14 @@ public class boost : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        //turn = Mathf.Deg2Rad * transform.rotation.y;
         turn = transform.rotation.y;
+        Debug.DrawLine(transform.position + new Vector3(0f, 0.5f, 0f), transform.position + new Vector3(Mathf.Sin(turn) * stren, 0.5f, Mathf.Cos(turn) * stren));
     }
 
     void OnTriggerStay(Collider other) {
         if (other.name == "PlayerBall") {
-            playerMove.rb.AddForce(Mathf.Cos(turn), 0f, Mathf.Sin(turn));
+            playerMove.rb.AddForce(Mathf.Sin(turn) * stren, 0f, Mathf.Cos(turn) * stren);
         }
     }
 }
