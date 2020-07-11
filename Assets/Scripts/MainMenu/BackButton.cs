6140f846c95bcc5a1f7b1ab049f3f517c55e563c
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 12:14:07 -0400

    no message

---
+++ Assets/Scripts/MainMenu/BackButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackButton : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite inactiveButton;
+    public Sprite hoveringButton;
+    public Sprite pressedButton;
+    private bool pressed;
+    public Animator animator;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        if (!pressed)
+        {
+            spriteRenderer.sprite = hoveringButton;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = inactiveButton;
+    }
+
+    void OnMouseDown()
+    {
+        pressed = true;
+        spriteRenderer.sprite = pressedButton;
+        animator.SetInteger("Menustate", 0);
+    }
+
+    void OnMouseUp()
+    {
+        spriteRenderer.sprite = hoveringButton;
+        pressed = false;
+    }
+}
