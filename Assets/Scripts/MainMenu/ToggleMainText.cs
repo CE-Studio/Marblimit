f892241a4cfecbaab61b0b387dacfff829887bf7
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 11:29:11 -0400

    no message

---
+++ Assets/Scripts/MainMenu/ToggleMainText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMainText : MonoBehaviour
+{
+    public static bool mainTextVisible = true;
+
+    void OnTriggerEnter(Collider MainCamera)
+    {
+        mainTextVisible = !mainTextVisible;
+    }
+}
