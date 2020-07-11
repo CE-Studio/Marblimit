98172ceaf6d14ca27a2b9462317cbf0550786138
clarence112 <clarencepacelli@gmail.com>
2020-07-11 13:17:45 -0400

    no message

---
+++ Assets/Scripts/spinRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinRandomizer : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        transform.position = new Vector3(Random.Range(-100f, 100f), 0.5f, Random.Range(-100f, 100f));
+        transform.Rotate(0f, Random.Range(0f, 360f), Mathf.Round(Random.Range(0, 1)) * 180f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
--- Assets/Scripts/win.cs
+++ Assets/Scripts/win.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class win : MonoBehaviour
 {
     public static bool hasWon = false;
+    public GameObject boost;
+    public GameObject lspin;
+    public GameObject sspin;
+    public GameObject lift;
+    public GameObject wall;
+    private float i = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,15 @@ public class win : MonoBehaviour
         if (other.name == "PlayerBall") {
             hasWon = true;
             playerMove.rspawn = true;
+            i = 0f;
+            while (i < 3) {
+                GameObject ObjectName = Instantiate(boost, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                GameObject ObjectNamee = Instantiate(lspin, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                GameObject ObjectNameee = Instantiate(sspin, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                GameObject ObjectNameeee = Instantiate(lift, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                GameObject ObjectNameeeee = Instantiate(wall, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+                i += Random.Range(0f, 1.0f);
+            }
         }
     }
 }
