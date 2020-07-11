9a3c00d99dff361e7159436b9e113c33d4f17598
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 01:31:43 -0400

    no message

---
+++ Assets/Scripts/MainMenu/OptionsButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsButton : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite inactiveButton;
+    public Sprite hoveringButton;
+    public Sprite pressedButton;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        spriteRenderer.sprite = hoveringButton;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = inactiveButton;
+    }
+
+    void OnMouseDown()
+    {
+        spriteRenderer.sprite = pressedButton;
+    }
+
+    void OnMouseUp()
+    {
+        spriteRenderer.sprite = hoveringButton;
+    }
+}
