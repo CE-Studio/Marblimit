e69545cf61d2d13bb3a57c2234fbb61390763cce
clarence112 <clarencepacelli@gmail.com>
2020-07-10 21:44:11 -0400

    no message

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -10,13 +10,16 @@ public class playerMove : MonoBehaviour {
     public static float fuel = 15.0f;
     private float HorizValueLastFrame;
     private float VertiValueLastFrame;
+    public static Vector3 spawnpoint;
+    public bool rspawn = false;
 
     void Start() {
-        
+        spawnpoint = transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate() {
+        checkRespawn();
         if (fuel > 0) {
             rb.AddForce(Input.GetAxis("Horizontal") * force, 0f, Input.GetAxis("Vertical") * force);
             if ((Input.GetAxis("Horizontal") != 0 && Mathf.Abs(Input.GetAxis("Horizontal")) >= HorizValueLastFrame) || (Input.GetAxis("Vertical") != 0 && Mathf.Abs(Input.GetAxis("Vertical")) >= VertiValueLastFrame)) {
@@ -24,6 +27,19 @@ public class playerMove : MonoBehaviour {
             }
             HorizValueLastFrame = Mathf.Abs(Input.GetAxis("Horizontal"));
             VertiValueLastFrame = Mathf.Abs(Input.GetAxis("Vertical"));
+        }
+        if (transform.position.y < -100f) {
+            rspawn = true;
+        }
+    }
+
+    void checkRespawn() {
+        if (rspawn) {
+            transform.position = spawnpoint;
+            rspawn = false;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            fuel = 15f;
         }
     }
 }
