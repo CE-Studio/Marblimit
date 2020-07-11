a9f07f4a290e13003b4c04ed16974d86299a78b2
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 14:01:06 -0400

    no message

---
+++ Assets/Scripts/LockMinimapCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMinimapCam : MonoBehaviour
+{
+    private Vector3 ballPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ballPos = GameObject.Find("PlayerBall").transform.position;
+        transform.position = ballPos + new Vector3(0, 30, 0);
+    }
+}
