bd007863cf26f7c747a70d9ffca11b4bcace6055
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 16:18:01 -0400

    no message

---
+++ Assets/Scripts/MainMenu/OptionsTextController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsTextController : MonoBehaviour
+{
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (ToggleOptionsText.optionsTextVisible)
+        {
+            transform.position = originalPosition;
+        }
+        else if (!ToggleOptionsText.optionsTextVisible)
+        {
+            transform.position = new Vector3(originalPosition.x, originalPosition.y - 20, originalPosition.z);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/ToggleOptionsText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOptionsText : MonoBehaviour
+{
+    public static bool optionsTextVisible = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            optionsTextVisible = !optionsTextVisible;
+        }
+    }
+}
