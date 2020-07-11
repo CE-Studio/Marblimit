680733d9fe419715f0eac5024ac874c4b5f448a9
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 01:27:18 -0400

    no message

--- Assets/Scripts/MainMenu/PlayButton.cs
+++ Assets/Scripts/MainMenu/PlayButton.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SpriteRenderer spriteRenderer;
+    public Sprite inactiveButton;
+    public Sprite hoveringButton;
+    public Sprite pressedButton;
+    
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseOver()
     {
-        
+        spriteRenderer.sprite = hoveringButton;
+    }
+
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = inactiveButton;
+    }
+
+    void OnMouseDown()
+    {
+        spriteRenderer.sprite = pressedButton;
+        SceneManager.LoadScene(sceneName:"Level");
+    }
+
+    void OnMouseUp()
+    {
+        spriteRenderer.sprite = hoveringButton;
     }
 }
