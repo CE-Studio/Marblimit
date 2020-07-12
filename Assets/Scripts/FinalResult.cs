6f4239943e2dd9ba167734973843bb9362634e19
EpsilonTheDerg <jamesclamey@gmail.com>
2020-07-11 20:55:53 -0400

    no message

---
+++ Assets/Scripts/FinalResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalResult : MonoBehaviour
+{
+    public TextMesh text;
+    
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+        if (win.iter == 1)
+        {
+            text.text = ("You cleared 1 iteration");
+        }
+        else
+        {
+            text.text = ("You cleared " + win.iter + " iterations");
+        }
+    }
+}
---
+++ Assets/Scripts/TimeoutTransitionInDed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeoutTransitionInDed : MonoBehaviour
+{
+    void Start()
+    {
+        transform.position += new Vector3(0, 0, Random.Range(-0.25f, 0.25f));
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, -0.05f, 0);
+    }
+}
